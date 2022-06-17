d2eaacc17378ea3b10d4a396af01a4ba9c32d48c
Brit <Brit@DESKTOP-BFORP22>
2022-06-16 17:03:04 -0700

    Add project files.

---
+++ ArithmeticOperators/Program.cs
@@ -0,0 +1,23 @@
+﻿int friends = 10;
+
+//friends = friends + 1;
+
+//friends += 2;
+
+//friends++;
+
+//friends = friends - 1;
+//friends -= 1;
+//friends--;
+
+//friends = friends * 2;
+//friends *= 2;
+
+//friends = friends / 2;
+//friends /= 2; 
+
+int remainder = friends % 3; 
+
+Console.WriteLine(remainder);
+
+Console.ReadKey();
